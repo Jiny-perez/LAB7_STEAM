21e30e1625c0c66826e9169292efb26942d123f8
EstebanFerS <estebanfernandez865@gmail.com>
2025-11-28 11:09:47 -0600

    Hola

---
+++ Steam/src/steam/Frames/UserFrame.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package steam.Frames;
+import javax.swing.*;
+/**
+ *
+ * @author esteb
+ */
+public class UserFrame extends JFrame{
+    public UserFrame(){
+        
+    }
+}
